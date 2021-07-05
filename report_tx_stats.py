afe8f8cb4a35d08b9c93cbb06ab1367353b0444b
Ian Coleman <ian@iancoleman.io>
2021-07-05 10:26:46 +1000

    Add liquidity bonus to dex
    Still a lot of work to do on the dex though, doesn't currently work
    correctly.

--- report_tx_stats.py
+++ report_tx_stats.py
@@ -1,17 +1,30 @@
+# Converts a list of transactions
+# in file all_maidsafecoin_txs.json
+# into a list of address balances
+# and reports stats on the balances.
+#
+# This is useful for auditing the transaction list for errors
+# and for taking snapshots of address balances at a certain
+# point in history.
+
 import json
 import math
 import sys
 
-MAID = 3
+# Option to exit the script as soon as anything goes wrong.
+# Sometimes you just want to run it all the way through even if things go
+# wrong, in which case this should be set to False
+exitOnAuditFailure = True
 
-f = open("all_maidsafecoin_txs.json")
-content = f.read()
-f.close()
-
-txs = json.loads(content)
-txs = sorted(txs, key=lambda t: (t["block"], int(t["txid"][:8],16)))
-
-print("Total txs: %s" % len(txs))
+###
+# DEX
+#
+# Orders are merely listed on the blockchain, matching is implied.
+# There's no transaction on the blockchin for a successful match between two
+# orders.
+# To track the flow of funds between matched orders we need to do our own dex
+# matching and output the transactions that would result from that matching.
+###
 
 class Dex:
 
@@ -34,6 +47,8 @@ class Dex:
         # and resolve ties with FIFO by keeping
         # the order chronological and using the first
         # instance of the best rate
+        # until the sell order is fully matched
+        # or there are no matches remaining
         transactions = []
         orderFullyMatched = self.isFullyMatched(order)
         while not orderFullyMatched:
@@ -66,6 +81,11 @@ class Dex:
             if bestMatchIndex is None:
                 break
             # calculate the transfer amounts
+            # TODO not sure if this is right
+            # What amounts to be using here?
+            # Docs are here:
+            # https://github.com/OmniLayer/spec/blob/fc37541641ad45959cdbb71a6985fcd99dae445c/OmniSpecification.adoc#723-sell-omni-protocol-coins-for-another-omni-protocol-currency
+            # Liquidity bonus maybe?
             bm = self.orders[bestMatchIndex]
             bmMatchedSale = 0
             bmMatchedDesired = 0
@@ -73,6 +93,7 @@ class Dex:
             bmDesiredRemaining = bm["desiredAmount"] - bm["desiredMatched"]
             orderDesiredRemaining = order["desiredAmount"] - order["desiredMatched"]
             orderSaleRemaining = order["saleAmount"] - order["saleMatched"]
+            liquidityBonus = round(bmDesiredRemaining * 0.003) # TODO how to use this?
             if bmSaleRemaining <= orderDesiredRemaining:
                 # earlier order sale amount is completely consumed
                 bmMatchedSale = bmSaleRemaining
@@ -138,12 +159,72 @@ class Dex:
         if not didCancel:
             print("No matching order to cancel for %s %s" % (address, txid))
 
+def runDexTests():
+    # Tests for dex order matching
+    def testOrderMatching():
+        dex = Dex()
+        # add order to buy 100 maidsafecoin for 1 omnicoin
+        #def add(self, address, desiredAmount, desiredPropertyId, saleAmount, salePropertyId):
+        dex.add("a", 100, 3, 1, 1)
+        # add another order to buy 10 omnicoin for 50 maidsafecoin
+        dex.add("b", 10, 1, 50, 3)
+        # should be no matched orders yet
+        for o in dex.orders:
+            assert(o["saleMatched"] == 0)
+            assert(o["desiredMatched"] == 0)
+    testOrderMatching()
+
+    # Tests from table starting with
+    # "The following table shows examples of the liquidity bonus based on the new order’s amount for sale and the existing order’s minimum amount desired, for indivisible coins."
+    # https://github.com/OmniLayer/spec/blob/fc37541641ad45959cdbb71a6985fcd99dae445c/OmniSpecification.adoc#723-sell-omni-protocol-coins-for-another-omni-protocol-currency
+    def omnitestsRow1():
+        dex = Dex()
+        dex.add("a", 1000, 3, 1, 1)
+        txs = dex.add("b", 1, 1, 1003, 3)
+        # liquidity bonus is 3
+        # 1000 + 3 is sent
+        for tx in txs:
+            if tx["propertyid"] == 3:
+                assert(tx["amount"] == "1003")
+        assert(len(txs) == 2)
+        # new order remainder for sale is 0
+        newOrderRemainder = dex.orders[1]["saleAmount"] - dex.orders[1]["saleMatched"]
+        assert(newOrderRemainder == 0)
+        # existing order remainder desired is 0
+        existingOrderRemainder = dex.orders[0]["desiredAmount"] - dex.orders[0]["desiredMatched"]
+        assert(existingOrderRemainder == 0)
+    omnitestsRow1()
+
+# These tests currently fail, so they need to be wrapped in the conditional.
+# In the future when dex tests pass this should be removed.
+if exitOnAuditFailure:
+    runDexTests()
+
+###
+# Data and variables
+###
+
+MAID = 3
+
+f = open("all_maidsafecoin_txs.json")
+content = f.read()
+f.close()
+
+txs = json.loads(content)
+txs = sorted(txs, key=lambda t: (t["block"], t["positioninblock"]))
+
 dex = Dex()
 
 balances = {
-    "1ARjWDkZ7kT9fwjPrjcQyvbXDkEySzKHwu": 1452520155,
+    "1ARjWDkZ7kT9fwjPrjcQyvbXDkEySzKHwu": 452520155,
 }
-totalcp = 0
+
+totalCrowdsalePurchases = 0
+
+###
+# Calculations based on transaction list
+###
+
 for tx in txs:
     if not tx["valid"]:
         continue
@@ -172,7 +253,7 @@ for tx in txs:
         addrDst = tx["sendingaddress"]
         amountSrc = tx["purchasedtokens"]
         amountDst = tx["purchasedtokens"]
-        totalcp = totalcp + float(amountDst)
+        totalCrowdsalePurchases = totalCrowdsalePurchases + float(amountDst)
     elif tx["type"] == "MetaDEx trade":
         # eg ada786071ea9f4c0c0f9a8ab7bde7e20160ec46ac7ae74c5c2f1127318e121b1
         address = tx["sendingaddress"]
@@ -223,12 +304,20 @@ for tx in txs:
     # check for negative balances
     if addrSrc in balances and balances[addrSrc] < 0:
         print("Addr just went to invalid negative balance %s: %s with tx %s" % (balances[addrSrc], addrSrc, tx["txid"]))
-        sys.exit(0)
+        if exitOnAuditFailure:
+            sys.exit(0)
     if addrDst in balances and balances[addrDst] < 0:
         print("Addr just went to invalid negative balance %s: %s with tx %s" % (balances[addrDst], addrDst, tx["txid"]))
-        sys.exit(0)
+        if exitOnAuditFailure:
+            sys.exit(0)
 
-print("Total maidsafecoin in crowdsale purchase: %s" % totalcp)
+###
+# Reporting
+###
+
+print("Total txs: %s" % len(txs))
+
+print("Total maidsafecoin in crowdsale purchase: %s" % totalCrowdsalePurchases)
 
 totalFromUtxos = 0
 for addr in balances:
@@ -250,8 +339,8 @@ for addr in balances:
 
 print("Total addrs: %s" % len(balances.keys()))
 print("Addrs with gt zero balance: %s" % gtz)
-print("Addrs with lt zero balance: %s" % ltz)
 print("Addrs with zero balance: %s" % z)
+print("Addrs with lt zero balance: %s" % ltz)
 
 for addr in ltzAddrs:
     print("lt 0 balance: %s %s" % (addr, balances[addr]))
@@ -272,20 +361,3 @@ for o in dex.orders:
         dexsells = dexsells + o["saleMatched"]
 print("Dex maidsafecoin waiting to be sold: %s" % dexsells)
 print("Dex maidsafecoin waiting to be bought: %s" % dexbuys)
-
-
-
-
-
-
-
-testdex = Dex()
-# add order to buy 100 maidsafecoin for 1 omnicoin
-#def add(self, address, desiredAmount, desiredPropertyId, saleAmount, salePropertyId):
-testdex.add("a", 100, 3, 1, 1)
-# add another order to buy 10 omnicoin for 50 maidsafecoin
-testdex.add("b", 10, 1, 50, 3)
-# should be no matched orders yet
-for o in testdex.orders:
-    assert(o["saleMatched"] == 0)
-    assert(o["desiredMatched"] == 0)
